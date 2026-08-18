e499c033e13bd98be3f01559dd44f3ab1378072a
Marken.Marine <mv.samarskiy@gmail.com>
2026-08-18 17:47:00 +0200

    Load client report v5

--- telegram_bootstrap.py
+++ telegram_bootstrap.py
@@ -28,13 +28,14 @@ install_session_routes(app, app_module)
 install_background_search_routes(app, app_module)
 
 
-RELEASE_MARKER = "v7.5-readable-report"
+RELEASE_MARKER = "v7.6-client-report"
 STREAM_CLIENT_TAG = '<script src="/static/streaming.js"></script>'
 RESOURCE_PROGRESS_TAG = '<script src="/static/resource_progress.js"></script>'
-SESSION_SYNC_TAG = '<script src="/static/session_sync.js"></script>'
+SESSION_SYNC_TAG = '<script src="/static/session_sync.js?v=5"></script>'
 BACKGROUND_SEARCH_TAG = '<script src="/static/background_search.js"></script>'
 AUDIT_REPORT_TAG = '<script src="/static/audit_report.js?v=4"></script>'
-REPORT_CONTROLS_TAG = '<script src="/static/report_controls.js?v=4"></script>'
+CLIENT_REPORT_TAG = '<script src="/static/client_report.js?v=5"></script>'
+REPORT_CONTROLS_TAG = '<script src="/static/report_controls.js?v=5"></script>'
 FEED_NAVIGATION_TAG = '<script src="/static/feed_navigation.js"></script>'
 
 
@@ -54,6 +55,8 @@ def prevent_stale_html(response):
             tags.append(BACKGROUND_SEARCH_TAG)
         if AUDIT_REPORT_TAG not in body:
             tags.append(AUDIT_REPORT_TAG)
+        if CLIENT_REPORT_TAG not in body:
+            tags.append(CLIENT_REPORT_TAG)
         if REPORT_CONTROLS_TAG not in body:
             tags.append(REPORT_CONTROLS_TAG)
         if FEED_NAVIGATION_TAG not in body:
